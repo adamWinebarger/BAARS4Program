e0be3b9b724e8a39f40576a192547573056da2ca
andre <andre@DESKTOP-Q108EDH>
2021-10-15 17:08:52 -0400

    Add project files.

---
+++ BAARS 4 Tester/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BAARS_4_Tester
+{
+    /* 
+     * Main Window for the BAARS 4 Tester
+     * */
+
+    public partial class MainWindow : Window
+    {
+
+        // Runs on start of window
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button2.Content = "Quickscore for \nParent/Teacher Forms";
+        }
+
+        // Button to get user to take test, opens user information input window
+        private void TakeTestButton_Click(object sender, RoutedEventArgs e)
+        {
+            new TesterInfoWindow().Show();
+        }
+
+        // Quickly calculate the score
+        private void QuickScoreButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
