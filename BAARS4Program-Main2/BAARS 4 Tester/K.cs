ffcd32d8bd2d44b9d6def006e75d40a9e52d52f4
adamWinebarger <50574306+adamWinebarger@users.noreply.github.com>
2021-11-09 23:15:17 -0500

    Add files via upload
    - Fixed the issue with the adult vs. youth testers
    - Added a few new buttons in for functionality.
    -...did some other stuff... I don't really remember what :/

---
+++ BAARS4Program-Main2/BAARS 4 Tester/K.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAARS_4_Tester
+{
+    static class K
+    {
+        //Text blocks for the sections of the test. Shown as Messageboxes in TestWindow
+        public const string adultDescription = "This is the adult description block",
+            youthDescription = "This is the youth description block";
+
+        public const string testerProfiles = "Tester_Profiles\\";
+
+    }
+}
